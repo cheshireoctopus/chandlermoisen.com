8d7411938ddb2991b3559c2d1ced7a568e3de852
cheshireoctopus <chandlermoisen@gmail.com>
2026-02-17 08:52:26 -0700

    Moves markdown posts into dedicated content directory; adds dynamic writing route to render blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/pages/writing' }),
+  loader: glob({ pattern: '**/*.md', base: './src/content/writing' }),
   schema: z.object({
     title: z.string(),
     date: z.string(),
